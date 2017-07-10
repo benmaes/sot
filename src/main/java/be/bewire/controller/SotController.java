bd54f71fd64ee53407f2bcbf84c1a3f6233ed7ab
Ben Maes <ben.maes@c4j.be>
2017-07-10 16:17:10 +0200

    prepared properties for message

--- src/main/java/be/bewire/controller/SotController.java
+++ src/main/java/be/bewire/controller/SotController.java
@@ -1,5 +1,6 @@
 package be.bewire.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SotController {
 
+    @Value("${message}")
+    private String message;
+
     @RequestMapping(method = RequestMethod.GET)
     public String message() {
-        return "Hello world!";
+        return message;
     }
 }
