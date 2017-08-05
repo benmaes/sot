650ca70075425d69b90cea527332d2f4a41256a3
Ben Maes <ben.maes@c4j.be>
2017-08-05 10:26:35 +0200

    removed unused import

--- src/main/java/be/bewire/model/Department.java
+++ src/main/java/be/bewire/model/Department.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import java.util.Date;
 
